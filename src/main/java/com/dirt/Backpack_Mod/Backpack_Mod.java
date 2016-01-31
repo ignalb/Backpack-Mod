7a35c72beefd47327bb521f0ec9c6798174570b3
Ignacio Pacheco <baxter.ignalb@gmail.com>
2016-01-31 17:22:59 -0500

    Created Main Class : Backpack_Mod.java

---
+++ src/main/java/com/dirt/Backpack_Mod/Backpack_Mod.java
@@ -0,0 +1,31 @@
+package com.dirt.Backpack_Mod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Backpack_Mod.ModID, name = Backpack_Mod.ModName, version = Backpack_Mod.ModVersion)
+public class Backpack_Mod {
+
+	public static final String ModID = "DIRT_Backpacks";
+	public static final String ModName = "Backpack Mod";
+	public static final String ModVersion = "0.1";
+	
+	@EventHandler
+	public void preinit(FMLPreInitializationEvent event){
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event){
+		
+	}
+	
+	@EventHandler
+	public void postinit(FMLPostInitializationEvent event){
+		
+	}
+	
+}
